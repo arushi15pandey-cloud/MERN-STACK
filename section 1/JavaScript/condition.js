c07152998b0677a0d5782d8b92506eced7565054
arushi15pandey-cloud <arushi15.pandey@gmail.com>
2026-08-04 18:28:57 +0530

    index.js

---
+++ section 1/JavaScript/condition.js
@@ -0,0 +1,16 @@
+num= 4;
+
+if(num%2===0){
+    console.log('even');
+   let  msg='hello';
+
+   const pi=3.14;
+
+   pi=456;
+}
+else{
+    console.log('odd') 
+} /*ctrl+c for runing terminal*/ 
+
+//console.log(msg);
+// ctrl +/ is used for comment
---
+++ section 1/JavaScript/index.js
@@ -0,0 +1,16 @@
+console.log('Hello Javascript Again');
+
+a=20;
+
+console.log(a);
+
+console.log(typeof a);
+
+username='Raju';
+
+console.log(typeof username);
+
+isHungry=false;
+
+console.log(typeof isHungry);
+
